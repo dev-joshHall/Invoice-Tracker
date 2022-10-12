cb14a5541cb8b6e1452be10a17c0150788a73ad1
Joshua Hall <jchcaleb@gmail.com>
2022-10-12 16:47:27 -0600

    added mongo playground file

---
+++ invoice-tracker/src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Nav from './Nav';
+
+function Main() {
+    return ( <div className='mainComponent bg-dark'>
+        <p>Main app</p>
+        <Nav/>
+    </div> );
+}
+
+export default Main;
---
+++ invoice-tracker/src/components/Nav.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+function Nav() {
+    return ( <div>
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Active</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+        </ul>
+
+
+    </div> );
+}
+
+export default Nav;
