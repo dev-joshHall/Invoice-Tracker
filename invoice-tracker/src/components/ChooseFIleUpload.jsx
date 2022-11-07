369ec8f2b820e6135d92a7cb5b0d46761ec6bc06
Joshua Hall <jchcaleb@gmail.com>
2022-11-07 14:00:48 -0700

    Added upload CSV file

--- invoice-tracker/src/components/ChooseFIleUpload.jsx
+++ invoice-tracker/src/components/ChooseFIleUpload.jsx
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import $ from 'jquery'
+import CSVReader from './CSVReader.tsx';
 
-function ChooseFileUpload() {
-    return ( <div className='border rounded d-flex flex-column bd-highlight mb-3'>
-        <span className='d-flex justify-content-center'>Step 1 - Choose File</span>
-        <span className='d-flex justify-content-center'>Upload File (CSV)</span>
-        <input type={"file"} className='d-flex justify-content-center'/>
-        <button className='btn btn-primary m-2'>Next</button>
-    </div> );
+class ChooseFileUpload extends Component {
+
+    componentDidMount = () => {
+        $('#step1NextBtn').on('click', this.nextPressed);
+    }
+
+    nextPressed = () => {
+        this.props.stateChanger({'uploadStep': 2});
+        this.props.updateProgBar();
+    }
+
+    render() {
+        return ( <div className='border rounded d-flex flex-column bd-highlight mb-3'>
+            <span className='d-flex justify-content-center'>Step 1 - Choose File</span>
+            <span className='d-flex justify-content-center'>Upload File (CSV)</span>
+            <div className='m-3'>
+                <CSVReader stateChanger={this.props.stateChanger} uploadState={this.props.uploadState}/>
+            </div>
+            <button id='step1NextBtn' className='btn btn-primary m-2' disabled={true}>Next</button>
+        </div> );
+    }
+    
 }
 
 export default ChooseFileUpload;
--- invoice-tracker/src/components/UploadProgress.jsx
+++ invoice-tracker/src/components/UploadProgress.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+
  function UploadProgress() {
+
     return ( <div className="d-flex flex-column bd-highlight mb-3">
         <span className='d-flex justify-content-center'>Bulk Uploader</span>
-        <img className='d-flex justify-content-center' src='upload_docs.png' alt='upload-image' id='updocs-img'/>
-        <span className='d-flex justify-content-center'>Progress Bar Here</span>
+        <img className='d-flex justify-content-center' src='/upload_docs.png' alt='upload-image' id='updocs-img'/>
+        <div className="progress">
+            <div id='uploadPBar' className="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
     </div> );
  }
  
